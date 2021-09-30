ea948f6b19204febf16fd30ea5c13044085ca416
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2021-09-30 14:04:41 +0200

    created barnResource.java

---
+++ tomtensDatabas/src/main/java/nu/te4/tomtensdatabas/resources/barnResource.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.te4.tomtensdatabas.resources;
+
+/**
+ *
+ * @author Zanders Koch
+ */
+@
+public class barnResource {
+    
+}
